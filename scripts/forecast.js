a6fb394747523a3045fc064f84dd4bcec354ae94
Radu Rentea <radu.rentea@gmail.com>
2019-06-13 13:35:45 +0300

    First commit

---
+++ scripts/forecast.js
@@ -0,0 +1,28 @@
+const key = '19FRmrEuOX5VxcmzadyAmxYNjfG7d0HB';
+
+// get weather information
+const getWeather = async (locationKey) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${locationKey}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+// get city information
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return (data[0]);
+};
+
+
